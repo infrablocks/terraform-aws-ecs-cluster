a16193d02292e9a6409b772bf2055c7fba8bb276
Ina Tsetsova <inatsetsova@gmail.com>
2019-06-25 16:36:28 +0100

    Updating policy resource when file changes

--- iam.tf
+++ iam.tf
@@ -8,9 +8,13 @@ resource "aws_iam_role" "cluster_instance_role" {
   }
 }
 
+data "template_file" "cluster_instance_policy" {
+  template = "${file("${path.module}/policies/cluster-instance-policy.json")}"
+}
+
 resource "aws_iam_policy" "cluster_instance_policy" {
   description = "cluster-instance-policy-${var.component}-${var.deployment_identifier}-${var.cluster_name}"
-  policy = "${coalesce(var.cluster_instance_iam_policy_contents, file("${path.module}/policies/cluster-instance-policy.json"))}"
+  policy = "${coalesce(var.cluster_instance_iam_policy_contents, data.template_file.cluster_instance_policy.rendered)}"
 }
 
 resource "aws_iam_policy_attachment" "cluster_instance_policy_attachment" {
