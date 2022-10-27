64f70a7cf6f69129f34d0a78ea7a08d2c7810299
Liam Griffin-Jowett <gryffskin@gmail.com>
2022-10-27 16:49:39 +0100

    Add more unit tests
    Authors: @Gryff @jonassvalin

--- spec/unit/autoscaling_group_spec.rb
+++ spec/unit/autoscaling_group_spec.rb
@@ -7,8 +7,14 @@ describe 'autoscaling group' do
     let(:component) do
       var(role: :root, name: 'component')
     end
-    let(:some_output) do
-      output(role: :prerequisites, name: 'certificate_arn')
+    let(:deployment_identifier) do
+      var(role: :root, name: 'deployment_identifier')
+    end
+    let(:cluster_name) do
+      var(role: :root, name: 'cluster_name')
+    end
+    let(:private_subnet_ids) do
+      output(role: :prerequisites, name: 'private_subnet_ids')
     end
 
     before(:context) do
@@ -27,10 +33,44 @@ describe 'autoscaling group' do
               .with_attribute_value(:min_size, 1))
     end
 
-    it 'outputs something important' do
+    it 'uses all private subnets' do
       expect(@plan)
-        .to(include_output_creation(name: 'important_output')
-              .with_value(36))
+        .to(include_resource_creation(type: 'aws_autoscaling_group')
+              .with_attribute_value(
+                :vpc_zone_identifier,
+                containing_exactly(*private_subnet_ids)
+              ))
+    end
+
+    it 'has tags' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_autoscaling_group')
+              .with_attribute_value(
+                :tag,
+                containing_exactly(
+                  {
+                    key: 'Name',
+                    propagate_at_launch: true,
+                    value: "cluster-worker-#{component}-" \
+                           "#{deployment_identifier}-default"
+                  },
+                  {
+                    key: 'ClusterName',
+                    propagate_at_launch: true,
+                    value: 'default'
+                  },
+                  {
+                    key: 'Component',
+                    propagate_at_launch: true,
+                    value: component
+                  },
+                  {
+                    key: 'DeploymentIdentifier',
+                    propagate_at_launch: true,
+                    value: deployment_identifier
+                  }
+                )
+              ))
     end
   end
 
@@ -48,64 +88,17 @@ describe 'autoscaling group' do
         .to(include_resource_creation(type: 'aws_autoscaling_group')
               .with_attribute_value(:min_size, 2))
     end
+
+    it 'uses the provided maximum cluster size' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_autoscaling_group')
+              .with_attribute_value(:max_size, 5))
+    end
+
+    it 'uses the provided desired cluster capacity' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_autoscaling_group')
+              .with_attribute_value(:desired_capacity, 3))
+    end
   end
 end
-#   subject(:auto_scaling_group) do
-#     autoscaling_group(output_for(:harness, 'autoscaling_group_name'))
-#   end
-#
-#   include_context 'terraform'
-#
-#   let(:component) { vars.component }
-#   let(:dep_id) { vars.deployment_identifier }
-#   let(:cluster_name) { vars.cluster_name }
-#
-#   it { is_expected.to exist }
-#   its(:min_size) { is_expected.to eq(vars.cluster_minimum_size.to_i) }
-#   its(:max_size) { is_expected.to eq(vars.cluster_maximum_size.to_i) }
-#
-#   its(:launch_configuration_name) do
-#     is_expected.to eq(output_for(:harness, 'launch_configuration_name'))
-#   end
-#
-#   its(:desired_capacity) do
-#     is_expected.to eq(vars.cluster_desired_capacity.to_i)
-#   end
-#
-#   it 'uses all private subnets' do
-#     expect(auto_scaling_group.vpc_zone_identifier.split(','))
-#       .to(contain_exactly(
-#             *output_for(:prerequisites, 'private_subnet_ids')
-#           ))
-#   end
-#
-#   it do
-#     expect(auto_scaling_group).to(have_tag('Name')
-#         .value("cluster-worker-#{component}-#{dep_id}-#{cluster_name}"))
-#   end
-#
-#   it { is_expected.to have_tag('Component').value(component) }
-#   it { is_expected.to have_tag('DeploymentIdentifier').value(dep_id) }
-#   it { is_expected.to have_tag('ClusterName').value(cluster_name) }
-#   it { is_expected.to have_tag('ImportantTag').value('important-value') }
-#
-#   context 'when scale in protection enabled' do
-#     before(:all) do
-#       reprovision(protect_cluster_instances_from_scale_in: 'yes')
-#     end
-#
-#     its(:new_instances_protected_from_scale_in) do
-#       is_expected.to(be(true))
-#     end
-#   end
-#
-#   context 'when scale in protection disabled' do
-#     before(:all) do
-#       reprovision(protect_cluster_instances_from_scale_in: 'no')
-#     end
-#
-#     its(:new_instances_protected_from_scale_in) do
-#       is_expected.to(be(false))
-#     end
-#   end
-# end
