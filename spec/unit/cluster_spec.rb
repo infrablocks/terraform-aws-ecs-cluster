cbbf47178fc3e0f9e0dcdd4f123a739eb44018bc
Liam Griffin-Jowett <gryffskin@gmail.com>
2022-10-28 16:24:08 +0100

    unit test cluster
    Authors: @Gryff @jonassvalin

--- spec/unit/cluster_spec.rb
+++ spec/unit/cluster_spec.rb
@@ -1,52 +1,43 @@
-# # frozen_string_literal: true
-#
-# require 'spec_helper'
-#
-# describe 'ECS Cluster' do
-#   subject(:ecs_cluster) do
-#     ecs_client.describe_clusters({
-#                                    clusters: ["#{vars.component}-" \
-#                                               "#{vars.deployment_identifier}-" \
-#                                               "#{vars.cluster_name}"],
-#                                    include: ['SETTINGS']
-#                                  }).clusters[0]
-#   end
-#
-#   include_context 'terraform'
-#
-#   it 'exists' do
-#     expect(ecs_cluster).not_to(be_nil)
-#   end
-#
-#   context 'when container insights enabled' do
-#     before(:all) do
-#       reprovision(
-#         enable_container_insights: 'yes'
-#       )
-#     end
-#
-#     it 'has container insights enabled on the cluster' do
-#       container_insights_setting = ecs_cluster.settings.find do |setting|
-#         setting[:name] == 'containerInsights'
-#       end
-#
-#       expect(container_insights_setting[:value]).to(eq('enabled'))
-#     end
-#   end
-#
-#   context 'when container insights disabled' do
-#     before(:all) do
-#       reprovision(
-#         enable_container_insights: 'no'
-#       )
-#     end
-#
-#     it 'has container insights disabled on the cluster' do
-#       container_insights_setting = ecs_cluster.settings.find do |setting|
-#         setting[:name] == 'containerInsights'
-#       end
-#
-#       expect(container_insights_setting[:value]).to(eq('disabled'))
-#     end
-#   end
-# end
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'ECS Cluster' do
+  before(:context) do
+    @plan = plan(role: :root)
+  end
+
+  it 'exists' do
+    expect(@plan)
+      .to(include_resource_creation(type: 'aws_ecs_cluster')
+            .once)
+  end
+
+  context 'when container insights enabled' do
+    before(:context) do
+      @plan = plan(role: :root) do |vars|
+        vars.enable_container_insights = 'yes'
+      end
+    end
+
+    it 'has container insights enabled on the cluster' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_ecs_cluster')
+              .with_attribute_value([:setting, 0, :value], 'enabled'))
+    end
+  end
+
+  context 'when container insights disabled' do
+    before(:context) do
+      @plan = plan(role: :root) do |vars|
+        vars.enable_container_insights = 'no'
+      end
+    end
+
+    it 'has container insights disabled on the cluster' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_ecs_cluster')
+              .with_attribute_value([:setting, 0, :value], 'disabled'))
+    end
+  end
+end
