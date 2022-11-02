a9b04eb4564cb38115c778246ee9f804a17441c5
Liam Griffin-Jowett <gryffskin@gmail.com>
2022-11-02 15:09:22 +0000

    unit test launch configuration

--- spec/integration/launch_configuration_spec.rb
+++ spec/integration/launch_configuration_spec.rb
@@ -11,39 +11,11 @@ describe 'Launch Configuration' do
 
   include_context 'terraform'
 
-  it { is_expected.to exist }
-  its(:instance_type) { is_expected.to eq(vars.cluster_instance_type) }
-
-  context 'when using latest amazon linux 2 optimised for ECS' do
-    let(:latest_amazon_linux_2_ecs_optimised_ami_id) do
-      response = ec2_client.describe_images(
-        {
-          owners: ['amazon'],
-          filters: [
-            {
-              name: 'name',
-              values: ['amzn2-ami-ecs-hvm-*-x86_64-ebs']
-            }
-          ]
-        }
-      )
-      most_recent_image = response.images.max_by do |image|
-        DateTime.parse(image.creation_date)
-      end
-
-      most_recent_image.image_id
-    end
-
-    its(:image_id) do
-      is_expected.to eq(latest_amazon_linux_2_ecs_optimised_ami_id)
-    end
-  end
-
-  it 'does not add a docker block device' do
+  it 'does not add a docker block device' do # does this test do anything?
     expect(launch_config.block_device_mappings.size).to(eq(1))
   end
 
-  context 'when custom security groups are provided' do
+  context 'when custom security groups are provided' do # integration
     before(:all) do
       security_group_ids =
         output_for(:prerequisites, 'security_group_ids')
@@ -70,52 +42,28 @@ describe 'Launch Configuration' do
     end
   end
 
-  context 'when AMIs specified' do
-    it 'uses provided image ID' do
-      ami_id = configuration.for(:harness).ami_id_in_region
-      puts ami_id
-      reprovision(
-        cluster_instance_amis:
-            "{#{vars.region}=\"#{ami_id}\"}"
-      )
-
-      expect(
-        launch_configuration(
-          output_for(:harness, 'launch_configuration_name')
-        )
-            .image_id
-      ).to eq(ami_id)
-    end
-  end
-
-  its(:key_name) do
+  its(:key_name) do # integration
     is_expected.to eq(
       "cluster-#{vars.component}-#{vars.deployment_identifier}-" \
       "#{vars.cluster_name}"
     )
   end
 
-  its(:iam_instance_profile) do
-    is_expected.to eq(
-      "cluster-instance-profile-#{vars.component}-" \
-      "#{vars.deployment_identifier}-#{vars.cluster_name}"
-    )
-  end
-
-  its(:user_data) do
+  its(:user_data) do # integration? base64 decoding doesn't seem
+    # to work in unit testing
     is_expected.to eq(Base64.strict_encode64(<<~DOC))
       #!/bin/bash
       echo "ECS_CLUSTER=#{vars.component}-#{vars.deployment_identifier}-#{vars.cluster_name}" > /etc/ecs/ecs.config
     DOC
   end
 
-  it {
+  it { # integration
     expect(launch_config).to have_security_group(
       "#{vars.component}-#{vars.deployment_identifier}-#{vars.cluster_name}"
     )
   }
 
-  describe 'launch config name' do
+  describe 'launch config name' do # prefix is unit tested, so do we need this?
     let(:launch_configuration_name) do
       output_for(:harness, 'launch_configuration_name')
     end
@@ -132,13 +80,5 @@ describe 'Launch Configuration' do
     it 'contains the cluster name' do
       expect(launch_configuration_name).to(match(/#{vars.cluster_name}/))
     end
-  end
-
-  it 'uses the specified size for the root block device' do
-    root_device_mapping = launch_config.block_device_mappings.find do |d|
-      d.device_name != '/dev/xvdcz'
-    end
-    expect(root_device_mapping.ebs.volume_size)
-      .to(eq(vars.cluster_instance_root_block_device_size.to_i))
   end
 end
